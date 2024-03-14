67c954b80f46e71ea7290682d091ab2a83d55779
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2024-03-14 23:58:59 +0900

    Feat: 스웨거 기본 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('co-KKIRI')
+    .setDescription('co-KKIRI API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  await app.listen(3000);
 }
+
 bootstrap();
