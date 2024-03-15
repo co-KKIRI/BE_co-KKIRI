c9b398f2e9f01dd05a06c408a1449f45df1612fa
kontae <koet0928@gmail.com>
2024-03-15 16:16:04 +0900

    Feat: TypeORM 및 env 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,17 @@
-import { Test } from './test/entities/test.entity';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TestModule } from './test/test.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeORMConfigService } from 'database.config';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   isGlobal: true,
-    //   envFilePath: '.env.dev'
-    // }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'co-kkiri.cvgmeiuqivj8.ap-northeast-2.rds.amazonaws.com',
-      port: 3306,
-      username: 'root',
-      password: 'qwer1234',
-      database: 'cokkiri',
-      entities: [Test],
-      synchronize: false,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useClass: TypeORMConfigService,
     }),
     TestModule,
   ],
