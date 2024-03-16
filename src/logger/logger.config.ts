91594d337a0ca7911d9161ff27027ade952b2b66
rockpell <aythffk@gmail.com>
2024-03-16 20:10:38 +0900

    Feat: 로거 설정에 request Id 들어가도록 수정

--- src/logger/logger.config.ts
+++ src/logger/logger.config.ts
@@ -1,46 +1,21 @@
-import { createLogger } from 'winston';
 import * as winston from 'winston';
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston/dist/winston.utilities';
-import { WinstonModule } from 'nest-winston';
-import winstonDaily from 'winston-daily-rotate-file';
+import { ClsService } from 'nestjs-cls';
 
-const isProduction = process.env['NODE_ENV'] === 'production';
-
-const productionFormat = winston.format.simple();
-
-const developmentFormat = winston.format.combine(
-  winston.format.timestamp(),
-  winston.format.ms(),
-  nestWinstonModuleUtilities.format.nestLike('co-KKIRI', {
-    colors: true,
-    prettyPrint: true,
-  }),
-);
-
-const logDir = __dirname + '/../../logs';
-const dailyOptions = (level: string) => {
-  return {
-    level,
-    datePattern: 'YYYY-MM-DD',
-    dirname: logDir + `/${level}`,
-    filename: `%DATE%.${level}.log`,
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d',
-  };
+const winstonLoggerTransport = (clsService: ClsService) => {
+  const format = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.ms(),
+    winston.format((info) => {
+      info.message = `[${clsService.getId()}] ${info.message}`;
+      return info;
+    })(),
+    nestWinstonModuleUtilities.format.nestLike('co-KKIRI', {
+      colors: true,
+      prettyPrint: true,
+    }),
+  );
+  return new winston.transports.Console({ format: format });
 };
 
-const winstonLogger = WinstonModule.createLogger({
-  instance: createLogger({
-    transports: [
-      new winston.transports.Console({
-        format: isProduction ? productionFormat : developmentFormat,
-      }),
-      new winstonDaily(dailyOptions('info')),
-      new winstonDaily(dailyOptions('warn')),
-      new winstonDaily(dailyOptions('error')),
-    ],
-  }),
-});
-
-export { winstonLogger };
+export { winstonLoggerTransport };
