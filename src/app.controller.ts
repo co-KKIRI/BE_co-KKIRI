815546fda62afd1c010e24d38fb7352f00ab51b6
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2024-03-16 14:16:42 +0900

    Feat: 프론트 세팅

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): any {
+    // return this.appService.getHello();
+    return { result: 123 };
   }
 }
