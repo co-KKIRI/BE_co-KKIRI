ddc3f59854bb96577452fe651633e42228dfab57
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2024-03-20 01:47:27 +0900

    Feat: 구글 오어스 api 스웨거 반영

--- src/controller/google-auth.controller.ts
+++ src/controller/google-auth.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { GoogleAuthGuard } from 'src/guard/google-auth.guard';
 
+@ApiTags('GoogleAuth')
 @Controller('auth/google')
 export class GoogleAuthenticationController {
   constructor() {}
 
+  @ApiOperation({ summary: '구글 로그인 시도' })
   @Get('/login')
   @UseGuards(GoogleAuthGuard)
   async handleLogin() {
@@ -14,7 +17,8 @@ export class GoogleAuthenticationController {
     };
   }
 
-  @Get('/redirect')
+  @ApiOperation({ summary: '구글 로그인 후 처리' })
+  @Post('/redirect')
   @UseGuards(GoogleAuthGuard)
   async handleRedirect() {}
 }
