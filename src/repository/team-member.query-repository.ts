d6cc5e5344c256af78841eafdafc7de65516c02a
rockpell <aythffk@gmail.com>
2024-03-20 01:59:10 +0900

    Feat: 스터디 신청자 목록 api에 pagination 적용

--- src/repository/team-member.query-repository.ts
+++ src/repository/team-member.query-repository.ts
@@ -5,26 +5,42 @@ import { DataSource } from 'typeorm';
 import { plainToInstance } from 'class-transformer';
 import { TeamMemberStatus } from '../entity/common/Enums';
 import { Member } from '../entity/member.entity';
+import { PaginationRequest } from '../common/pagination/pagination-request';
 
 @Injectable()
 export class TeamMemberQueryRepository {
   constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
 
-  async getAllTeamMembers(postId: number, status: TeamMemberStatus) {
-    const teamMembers = await this.dataSource
-      .createQueryBuilder()
-      .from(TeamMember, 'team_member')
-      .innerJoin(Member, 'member', 'team_member.memberId = member.id')
-      .where('team_member.postId = :postId', { postId })
-      .andWhere('team_member.status = :status', { status })
+  async getAllTeamMembers(
+    postId: number,
+    status: TeamMemberStatus,
+    paginationRequest: PaginationRequest,
+  ): Promise<GetAllTeamMembersTuple[]> {
+    const teamMembers = await this.getBaseQuery(postId, status)
       .select([
         'team_member.id as teamMemberId',
         'team_member.memberId as memberId',
         'member.nickname as nickname',
         'member.profileImageUrl as profileImageUrl',
       ])
+      .skip(paginationRequest.skip)
+      .take(paginationRequest.take)
+      .orderBy('team_member.updatedAt', paginationRequest.order)
       .getRawMany();
     return plainToInstance(GetAllTeamMembersTuple, teamMembers);
+  }
+
+  async getAllTeamMembersTotalCount(postId: number, status: TeamMemberStatus): Promise<number> {
+    return await this.getBaseQuery(postId, status).getCount();
+  }
+
+  private getBaseQuery(postId: number, status: TeamMemberStatus) {
+    return this.dataSource
+      .createQueryBuilder()
+      .from(TeamMember, 'team_member')
+      .innerJoin(Member, 'member', 'team_member.memberId = member.id')
+      .where('team_member.postId = :postId', { postId })
+      .andWhere('team_member.status = :status', { status });
   }
 }
 
