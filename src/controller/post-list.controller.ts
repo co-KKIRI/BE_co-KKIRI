981194668f5b98dd5826f68532d091f1c9358f4a
Kontae <koet0928@gmail.com>
2024-03-21 13:17:40 +0900

    refactor: 스터디 목록 리팩토링

--- src/controller/post-list.controller.ts
+++ src/controller/post-list.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
 import { ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { PostListResponse } from "src/dto/response/post-list-response";
 import { PostListService } from "src/service/post-list.service";
 
 @ApiTags('PostList')
-@Controller('post')
+@Controller()
 export class PostListController {
-  constructor(private readonly postListService: PostListService) { }
+  constructor(private readonly postListService: PostListService,) { }
 
   @ApiOperation({ summary: '스터디 목록' })
   @ApiCreatedResponse({ type: PostListResponse })
-  @Get('/list')
+  @Get('post/list')
   async getPostList(): Promise<PostListResponse> {
     const postList = await this.postListService.getPostList();
     return PostListResponse.from(postList);
   }
+
+
 }
