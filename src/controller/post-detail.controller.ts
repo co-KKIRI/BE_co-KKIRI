48040592977bd5985bd6bb66a63a26d2ed01aa40
Kontae <koet0928@gmail.com>
2024-03-23 03:41:26 +0900

    Feat: 스터디 지원하기 api 추가

--- src/controller/post-detail.controller.ts
+++ src/controller/post-detail.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, UseGuards } from "@nestjs/common";
 import { ApiCreatedResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { ApiPaginatedResponse } from "src/common/pagination/pagination.decorator";
 import { PostCommentResponse } from "src/dto/response/post-comment.response";
 import { PostDetailResponse } from "src/dto/response/post-detail.response";
+import { RolesGuard } from "src/guard/roles.guard";
 import { PostDetailService } from "src/service/post-detail.service";
+
 
 @ApiTags('PostDetail')
 @Controller()
+@UseGuards(RolesGuard)
+
 export class PostDetailController {
   constructor(private readonly postDetailService: PostDetailService) { }
 
@@ -25,6 +29,16 @@ export class PostDetailController {
     const commentInfo = await this.postDetailService.getPostComments(postId);
     return PostCommentResponse.from(commentInfo);
 
+  }
+
+  @ApiOperation({ summary: '포스트 지원하기' })
+  @Post('post/:postId/apply')
+  async applyPost(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Req() req): Promise<void> {
+    console.log(req.user);
+    await this.postDetailService.applyPost(postId, req.user.id);
+    
   }
 
 }
--- src/service/post-detail.service.ts
+++ src/service/post-detail.service.ts
@@ -1,14 +1,17 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { ConflictException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { GetPostComment, GetPostCommentDto } from "src/dto/get-post-comment.dto";
 import { GetPostDetailDto } from "src/dto/get-post-detail.dto";
+import { TeamMemberStatus } from "src/entity/common/Enums";
 import { Post } from "src/entity/post.entity";
+import { TeamMember } from "src/entity/team-member.entity";
 import { PostDetailQueryRepository } from "src/repository/post-detail.query-repository";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class PostDetailService {
   constructor(@InjectRepository(Post) private readonly postRepository: Repository<Post>,
+    @InjectRepository(TeamMember) private readonly teamMemberRepository: Repository<TeamMember>,
     private readonly postDetailQueryRepository: PostDetailQueryRepository) { }
 
   async getPostDetail(postId: number): Promise<GetPostDetailDto> {
@@ -27,5 +30,25 @@ export class PostDetailService {
       //TODO : 유저 검증 필요
       GetPostComment.from(postComment, post.id));
     return getPostComments;
+  }
+
+  async applyPost(postId: number, memberId: number): Promise<void> {
+    const post = await this.postRepository.findOneBy({ id: postId });
+    if (post === null) {
+      throw new NotFoundException('해당 포스트를 찾을 수 없습니다.');
+    }
+    const isAlreadyTeamMember = await this.teamMemberRepository.findOneBy({
+      postId: postId, memberId: memberId
+    });
+    
+    if (isAlreadyTeamMember !== null) {
+      throw new ConflictException('해당 포스트에 이미 존재하는 유저입니다.');
+    }
+
+    await this.teamMemberRepository.save({
+      postId: postId,
+      memberId: memberId,
+      status: TeamMemberStatus.READY,
+    })
   }
 }
