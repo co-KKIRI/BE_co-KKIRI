bc63b24bb678274146d3cf168b3af98e86d21232
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2024-03-23 01:54:52 +0900

    Feat: MemberQueryRepository 추가

--- src/service/member.service.ts
+++ src/service/member.service.ts
@@ -5,10 +5,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Member } from 'src/entity/member.entity';
 import { SocialLoginDto } from 'src/dto/socialLoginDto';
 import { GetMemberInfoSummaryResponse } from 'src/dto/response/get-member-info-summary.response';
+import { MemberQueryRepository } from 'src/repository/member.query-repository';
 
 @Injectable()
 export class MemberService {
-  constructor(@InjectRepository(Member) private memberRepository: Repository<Member>) {
+  constructor(
+    @InjectRepository(Member) private memberRepository: Repository<Member>,
+    private readonly memberQueryRepository: MemberQueryRepository,
+  ) {
     // private readonly configService: ConfigService,
   }
 
@@ -27,9 +31,7 @@ export class MemberService {
   }
 
   async getMemberInfoSummary(id: number): Promise<GetMemberInfoSummaryResponse> {
-    const memberSummary = await this.memberRepository.findOneBy({
-      id,
-    });
+    const memberSummary = await this.memberQueryRepository.getMemberInfoSummary(id);
 
     if (memberSummary === null) {
       throw new NotFoundException('해당 유저를 찾을 수 없습니다.');
