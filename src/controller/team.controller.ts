592754ced7d9bc333a82c06a16ea9e30993951c3
cleaninglab-jayd <jayd@cleaninglab.co.kr>
2024-03-25 00:07:11 +0900

    Feat: 팀 초대 조회에 teamMemberId 추가

--- src/controller/team.controller.ts
+++ src/controller/team.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Query, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetTeamInviteInfoResponse } from 'src/dto/response/team/get-team-invite-info.response';
 import { RolesGuard } from 'src/guard/roles.guard';
@@ -18,90 +18,4 @@ export class TeamController {
   ): Promise<GetTeamInviteInfoResponse> {
     return this.teamService.getTeamInviteInfo(req.user.id, teamInviteId);
   }
-
-  // @ApiOperation({ summary: '유저 정보' })
-  // @Get('/info')
-  // async getMyInfo(@Req() req): Promise<GetMyPageInfoResponse> {
-  //   return this.mypageService.getMyPageInfo(req.user.id);
-  // }
-
-  // @ApiOperation({ summary: '유저 정보 수정' })
-  // @Patch('/info')
-  // async patchMyInfo(@Req() req, @Body() memberInfo: PatchMyPageInfoDto): Promise<void> {
-  //   return this.mypageService.patchMyPageInfo(req.user.id, memberInfo);
-  // }
-
-  // @ApiOperation({ summary: '팀 초대된 목록' })
-  // @Get('/invite/list')
-  // async getMyPageInviteList(
-  //   @Req() req,
-  //   @Query() paginationRequest: PaginationRequest,
-  // ): Promise<PaginationResponse<GetMyPageInviteResponse>> {
-  //   const { inviteList, totalCount } = await this.mypageService.getMyPageInviteList(req.user.id, paginationRequest);
-
-  //   return PaginationResponse.of({
-  //     data: inviteList,
-  //     options: paginationRequest,
-  //     totalCount,
-  //   });
-  // }
-
-  // @ApiOperation({ summary: '스크랩 목록' })
-  // @Get('/scrap/list')
-  // async getMyScrapList(
-  //   @Req() req,
-  //   @Query() paginationRequest: PaginationRequest,
-  // ): Promise<PaginationResponse<GetMyPageScrapResponse>> {
-  //   const { myPageScrapList, totalCount } = await this.mypageService.getMyPageScrapList(req.user.id, paginationRequest);
-
-  //   return PaginationResponse.of({
-  //     data: myPageScrapList,
-  //     options: paginationRequest,
-  //     totalCount,
-  //   });
-  // }
-
-  // @ApiOperation({ summary: '프로필 공개 여부' })
-  // @Get('/visible-profile')
-  // async getMyPageVisibleProfile(@Req() req): Promise<GetMyPageVisibleProfileResponse> {
-  //   return this.mypageService.getMyPageVisibleProfile(req.user.id);
-  // }
-
-  // @ApiOperation({ summary: '프로필 공개 여부 수정' })
-  // @Patch('/visible-profile')
-  // async patchVisibleProfile(@Req() req, @Body() body: PatchMyPageVisibleProfileRequest): Promise<void> {
-  //   return this.mypageService.patchVisibleProfile(req.user.id, body.isVisibleProfile);
-  // }
 }
-
-// @ApiTags('PostTeamMember')
-// @Controller()
-// export class PostTeamMemberController {
-//   constructor(private readonly postTeamMemberService: PostTeamMemberService) {}
-
-//   @ApiOperation({ summary: '스터디/프로젝트 신청 수락' })
-//   @Patch('/post/team-member/:teamMemberId/accept')
-//   async acceptTeamMember(@Param('teamMemberId', ParseIntPipe) teamMemberId: number): Promise<void> {
-//     await this.postTeamMemberService.acceptTeamMember(teamMemberId);
-//   }
-
-//   @ApiOperation({ summary: '스터디/프로젝트 신청 거절' })
-//   @Patch('/post/team-member/:teamMemberId/reject')
-//   async rejectTeamMember(@Param('teamMemberId', ParseIntPipe) teamMemberId: number): Promise<void> {
-//     await this.postTeamMemberService.rejectTeamMember(teamMemberId);
-//   }
-
-//   @ApiOperation({ summary: '스터디/프로젝트 멤버 목록' })
-//   @ApiCreatedResponse({ type: PostTeamMemberResponse })
-//   @Get('/post/:postId/team-member')
-//   async getAllPostTeamMember(@Param('postId', ParseIntPipe) postId: number): Promise<PostTeamMemberResponse> {
-//     const getPostTeamMemberDto = await this.postTeamMemberService.getAllPostTeamMember(postId);
-//     return PostTeamMemberResponse.from(getPostTeamMemberDto);
-//   }
-
-//   @ApiOperation({ summary: '스터디/프로젝트 현재 팀원 삭제' })
-//   @Delete('/post/team-member/:teamMemberId')
-//   async deleteTeamMember(@Param('teamMemberId', ParseIntPipe) teamMemberId: number): Promise<void> {
-//     await this.postTeamMemberService.deleteTeamMember(teamMemberId);
-//   }
-// }
--- src/repository/team-invite.query-repository.ts
+++ src/repository/team-invite.query-repository.ts
@@ -3,9 +3,7 @@ import { InjectDataSource } from '@nestjs/typeorm';
 import { TeamMember } from '../entity/team-member.entity';
 import { DataSource } from 'typeorm';
 import { plainToInstance } from 'class-transformer';
-import { TeamMemberStatus } from '../entity/common/Enums';
 import { Member } from '../entity/member.entity';
-import { PaginationRequest } from '../common/pagination/pagination-request';
 import { TeamInvite } from 'src/entity/team-invite.entity';
 import { Post } from 'src/entity/post.entity';
 
@@ -22,6 +20,7 @@ export class TeamInviteQueryRepository {
         'p.id as postId',
         'p.title as postTitle',
         'ti.message as message',
+        'tm.id as teamMemberId',
       ])
       .from(TeamInvite, 'ti')
       .innerJoin(TeamMember, 'tm', 'ti.id = tm.teamInviteId')
@@ -33,48 +32,7 @@ export class TeamInviteQueryRepository {
 
     return plainToInstance(GetTeamInviteInfoTuple, teamInviteInfo);
   }
-
-  // async getAllTeamMembers(
-  //   postId: number,
-  //   status: TeamMemberStatus,
-  //   paginationRequest: PaginationRequest,
-  // ): Promise<GetAllTeamMembersTuple[]> {
-  //   const teamMembers = await this.getBaseQuery(postId, status)
-  //     .select([
-  //       'team_member.id as teamMemberId',
-  //       'team_member.memberId as memberId',
-  //       'member.nickname as nickname',
-  //       'member.position as position',
-  //       'member.profileImageUrl as profileImageUrl',
-  //     ])
-  //     .limit(paginationRequest.take)
-  //     .offset(paginationRequest.getSkip())
-  //     .orderBy('team_member.updatedAt', paginationRequest.order)
-  //     .getRawMany();
-  //   return plainToInstance(GetAllTeamMembersTuple, teamMembers);
-  // }
-
-  // async getAllTeamMembersTotalCount(postId: number, status: TeamMemberStatus): Promise<number> {
-  //   return await this.getBaseQuery(postId, status).getCount();
-  // }
-
-  // private getBaseQuery(postId: number, status: TeamMemberStatus) {
-  //   return this.dataSource
-  //     .createQueryBuilder()
-  //     .from(TeamMember, 'team_member')
-  //     .innerJoin(Member, 'member', 'team_member.memberId = member.id')
-  //     .where('team_member.postId = :postId', { postId })
-  //     .andWhere('team_member.status = :status', { status });
-  // }
 }
-
-// export class GetAllTeamMembersTuple {
-//   teamMemberId!: number;
-//   memberId!: number;
-//   nickname?: string;
-//   position?: string;
-//   profileImageUrl?: string;
-// }
 
 export class GetTeamInviteInfoTuple {
   sendMemberId: number;
@@ -82,4 +40,5 @@ export class GetTeamInviteInfoTuple {
   postId: string;
   postTitle: string;
   message: string;
+  teamMemberId: number;
 }
