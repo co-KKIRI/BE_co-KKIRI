3a1e5bbe4e93dd42502f9489843ff5fc19e40b83
rockpell <aythffk@gmail.com>
2024-03-25 23:15:30 +0900

    Chore: 주석 제거

--- src/service/member.service.ts
+++ src/service/member.service.ts
@@ -12,23 +12,7 @@ export class MemberService {
   constructor(
     @InjectRepository(Member) private memberRepository: Repository<Member>,
     private readonly memberQueryRepository: MemberQueryRepository,
-  ) {
-    // private readonly configService: ConfigService,
-  }
-
-  async createSocialUser(socialLoginInfoDto: SocialLoginDto): Promise<Member> {
-    // const { email, firstName, lastName, socialProvider, externalId, refreshToken } = socialLoginInfoDto;
-
-    const newMember: Member = await this.memberRepository.save({
-      // email: email,
-      // firstName: firstName,
-      // lastName: lastName,
-      // socialProvider: socialProvider,
-      // externalId: externalId,
-      // socialProvidedRefreshToken: refreshToken,
-    });
-    return await this.memberRepository.save(newMember);
-  }
+  ) {}
 
   async getMemberInfoSummary(id: number): Promise<GetMemberInfoSummaryResponse> {
     const memberSummary = await this.memberQueryRepository.getMemberInfoSummary(id);
@@ -41,38 +25,6 @@ export class MemberService {
 
     return new GetMemberInfoSummaryResponse(nickname, profileImageUrl);
   }
-
-  // async findUserById(id: number): Promise<User> {
-  //   return await this.userRepository.findOne({
-  //     where: {
-  //       id: id,
-  //     },
-  //   });
-  // }
-
-  // async updateSocialUserInfo(id: number) {
-  //   await this.userRepository.update(id, {
-  //     isSocialAccountRegistered: true,
-  //   });
-  //   const updateUser = await this.userRepository.findOne({
-  //     where: {
-  //       id: id,
-  //     },
-  //   });
-  //   return updateUser;
-  // }
-
-  // async updateSocialUserRefToken(id: number, refreshToken: string) {
-  //   await this.userRepository.update(id, {
-  //     socialProvidedRefreshToken: refreshToken,
-  //   });
-  //   const updateUser = await this.userRepository.findOne({
-  //     where: {
-  //       id: id,
-  //     },
-  //   });
-  //   return updateUser;
-  // }
 
   async getMember(memberId: number) {
     const member = await this.memberRepository.findOneBy({ id: memberId });
